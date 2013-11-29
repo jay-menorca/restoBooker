91ca14a39d2cb6621ebd9cfa4a1afa0e28306bf4
jay-menorca <zai_menorca@yahoo.com>
2013-11-29 14:21:10 -0500

    restoBooker v0.2: Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestoBooker::Application.config.secret_key_base = '38b37fa3aef72116f4589e5e200be23258f1fdfab45ef432afad2efcb83c413b56a070a1b46e2c302a200dd04cc8429d53d250f87c5e4960a12f7a54864fb97f'
